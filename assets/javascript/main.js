74c2634a0ff026b28df206124815602f9599c951
Kelvin Afrane <129594460+khayb1@users.noreply.github.com>
2024-10-14 17:00:56 +0000

    Add files via upload

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -42,3 +42,17 @@ function toggleContent(element) {
         plusIcon.classList.add('plus-icon');
     }
 }
+
+// scroll animation 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
